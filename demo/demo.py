f5112fda601a2d7c59c3575038b0f7aee723a577
oulutan <ulutan@ece.ucsb.edu>
2019-07-10 17:03:22 -0700

    testing installation stuff

---
+++ demo/demo.py
@@ -0,0 +1,18 @@
+from maskrcnn_benchmark.config import cfg
+from predictor import COCODemo
+
+config_file = "demo/e2e_faster_rcnn_X_101_32x8d_FPN_1x_custom.yaml"
+
+# update the config options with the config file
+cfg.merge_from_file(config_file)
+# manual override some options
+#cfg.merge_from_list(["MODEL.DEVICE", "cpu"])
+
+coco_demo = COCODemo(
+    cfg,
+    min_image_size=800,
+    confidence_threshold=0.7,
+)
+# load image and then run prediction
+image = cv2.imread('test_img.jpg')
+predictions = coco_demo.run_on_opencv_image(image)
